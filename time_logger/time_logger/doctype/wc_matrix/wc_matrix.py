8803ce0aae73a803d15e54198ff62c686b9fabf2
ketakid-rgb <ketakid@ithena.ai>
2026-04-09 09:37:59 +0000

    Data Model

---
+++ time_logger/time_logger/doctype/wc_matrix/wc_matrix.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2026, ketaki and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WCMatrix(Document):
+	pass
+
